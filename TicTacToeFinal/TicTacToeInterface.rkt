03bbabeda94ef44b63485ce12044a9eb2590f33d
Ema0898 <aguisnchez@gmail.com>
2018-05-07 17:52:11 -0600

    Integra todos los componentes
    Integra la interfaz con el algoritmo voraz y agrega los mensajes cuando la partida termina. Ya se que el jugador perdió, ganó o empató

---
+++ TicTacToeFinal/TicTacToeInterface.rkt
@@ -0,0 +1,179 @@
+#lang racket
+
+;; Importa todas las 
+(require 2htdp/image)
+(require 2htdp/universe)
+(require lang/posn)
+(require "MatrixManagment.rkt")
+(require "Game.rkt")
+(require "GreedyAlgorithm.rkt")
+
+(define WIDTH 400)
+(define HEIGHT 400)
+(define ROW 1)
+(define COLUMN 1)
+(define TURN 0)
+(define RT_STOP (list 0))
+(define MSG_POS (make-posn (- (/ WIDTH 2) 200) (/ HEIGHT 2)))
+
+(define CANVAS (empty-scene WIDTH HEIGHT))
+(define (get-x-position x m) (quotient x (floor (/ WIDTH m))))
+(define (get-y-position y n) (quotient y (floor (/ HEIGHT n))))
+(define square-size-x (/ WIDTH COLUMN))
+(define square-size-y (/ HEIGHT ROW))
+
+(define (empty-rectangle width height)
+  (rectangle width height "outline" "blue")
+)
+
+(define (o-rectangle width height)
+  (overlay (rectangle width height "outline" "blue") (circle 10 "solid" "red"))
+)
+
+(define (x-rectangle width height)
+  (overlay (rectangle width height "outline" "blue") (overlay (line -30 -40 "blue") (line -30 40 "blue")))
+)
+
+(define (win-msg player)
+  (cond    
+    ((equal? TURN (* ROW COLUMN))
+     (text "Bien jugado \nHemos empatado" 24 "blue")        
+    )
+    ((equal? (remainder player 2) 1)
+     (text "Felicidades \nMe has ganado la partida!" 24 "blue")
+    )
+    (else
+     (text "Perdiste \nNo te preocupes, no es facíl ganarme" 24 "blue")
+    )
+  )
+)
+
+(define (create-image-list datalist)
+  (create-image-list_aux datalist '())
+)  
+
+(define (create-image-list_aux datalist result)
+  (cond
+    ((empty? datalist)
+     result
+    )
+    (else
+     (cond
+       ((zero? (car datalist))
+        (create-image-list_aux (cdr datalist) (append result (list (empty-rectangle square-size-x square-size-y))))
+       )
+       ((equal? (car datalist) 1)
+        (create-image-list_aux (cdr datalist) (append result (list (o-rectangle square-size-x square-size-y))))
+       )
+       ((equal? (car datalist) 2)
+        (create-image-list_aux (cdr datalist) (append result (list (x-rectangle square-size-x square-size-y))))
+       )
+       (else
+        (create-image-list_aux (cdr datalist) result)
+       ) 
+     )  
+    ) 
+  )  
+)
+
+(define (create-image-position datamatrix)
+  (create-image-position_aux datamatrix 0 0 '())
+)
+
+(define (create-image-position_aux datamatrix i j result)
+  (cond
+    ((empty? datamatrix)
+     result
+    )
+    ((empty? (car datamatrix))
+     (create-image-position_aux (cdr datamatrix) (+ i square-size-y) 0 result)
+    )
+    (else
+     (create-image-position_aux (append (list (cdar datamatrix)) (cdr datamatrix)) i (+ square-size-x j)
+           (append result (list (make-posn j i))))
+    ) 
+  ) 
+)  
+
+(define (draw-grid n)
+  (cond
+    ((zero? (car RT_STOP))     
+     (place-images/align (create-image-list (matrix-to-list n)) (create-image-position n) "left" "top" CANVAS)
+    )
+    (else     
+     (place-images/align (append (create-image-list (matrix-to-list n)) (list (win-msg TURN)))
+                         (append (create-image-position n) (list MSG_POS)) "left" "top" CANVAS)
+    )
+  )  
+)
+  
+(define (process-player-action w x y me)
+  (cond
+    ((mouse=? me "button-down")
+     (cond
+       ((zero? (get_element w (get-y-position y ROW) (get-x-position x COLUMN)))
+        (set! TURN (+ TURN 1))
+        (replace_matrix w (get-y-position y ROW) (get-x-position x COLUMN) 1)
+       )
+       (else
+        w
+       ) 
+     )  
+    )
+    (else
+     w
+    )
+  )  
+)
+
+(define (restart w ke)
+  (cond
+    ((key=? ke "up")
+     (make_matrix ROW COLUMN)
+    )
+    (else
+     w
+    )
+  )  
+)
+
+(define (should-win? matrix)
+  (cond
+    ((or (win? matrix 1) (win? matrix 2) (equal? TURN (* ROW COLUMN)))
+     (set! RT_STOP (list 1))
+     #t
+    )
+    (else
+     #f
+    )
+  )
+)
+
+(define (pc-move matrix)
+  (cond
+    ((equal? (remainder TURN 2) 1)
+     (set! TURN (+ TURN 1))
+     (greedyAlgorithmn matrix)
+    )
+    (else
+     matrix
+    )
+  )
+)
+
+(define (TTT m n)
+  (set! ROW m)
+  (set! COLUMN n)
+  (set! square-size-x (/ WIDTH COLUMN))
+  (set! square-size-y (/ HEIGHT ROW))
+  
+  (big-bang
+      (make_matrix ROW COLUMN)
+      (name "Tic-Tac-Toe")      
+      (on-mouse process-player-action)      
+      (on-key restart)
+      (on-tick pc-move)
+      (to-draw draw-grid)
+      (stop-when should-win? draw-grid)      
+  )    
+)
